762a5785ee1b3c40c52dc44fcc245fe6112db0ab
Xin Zhi <115723006+xong002@users.noreply.github.com>
2023-08-01 14:01:28 +0800

    add hidden

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'day31lesson';
-
   updatedNumber: number;
+  leftEnabled: boolean = false;
+  rightEnabled: boolean = false;
+
   changedNumber(eventNumber: number){
     this.updatedNumber = eventNumber;
     console.log('parent number: ' + this.updatedNumber);
+  }
+
+  toggleLeft(){
+    this.leftEnabled = !this.leftEnabled;
+  }
+
+  toggleRight(){
+    this.rightEnabled = !this.rightEnabled;
   }
 }
--- src/app/data-display/data-display/data-display.component.ts
+++ src/app/data-display/data-display/data-display.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-data-display',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-display.component.css']
 })
 export class DataDisplayComponent {
-
+  @Input() numberInput: number;
 }
